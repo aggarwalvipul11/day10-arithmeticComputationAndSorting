4900a5e3549617d952accf1c2eae5559a7ed53e0
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-05 19:34:54 +0530

    Completed Usercase07 - Read values from dictionary into arrays.

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 
-#UserCase4 - Store the results in a Dictionary for every computation."
+#UserCase7 - Read the values from the Dictionary into the array.
 read -p "Enter 3 number for A, B and C " a b c
 declare -A resultDic
+
+count=0
 
 resultDic[op1]=$(($a+$b*$c))
 resultDic[op2]=$(($a*$b+$c))
 resultDic[op3]=$(($c+$a/$b))
 resultDic[op4]=$(($a%$b+$c))
 
+for calc in ${!resultDic[@]}
+do
+        a[((count++))]=${resultDic[$calc]}
+done
