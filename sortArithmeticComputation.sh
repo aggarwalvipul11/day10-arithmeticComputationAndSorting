15825e81995456b41267f841086f498e108ff85b
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-05 20:04:20 +0530

    Completed Usercase09

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -x
 
-#UserCase8 - Sort the results to show computation result in the descending order..
 read -p "Enter 3 number for A, B and C " a b c
 
 declare -A resultDic
-
 count=0
-
 resultDic[op1]=$(($a+$b*$c))
 resultDic[op2]=$(($a*$b+$c))
 resultDic[op3]=$(($c+$a/$b))
@@ -20,3 +17,5 @@ done
 echo ${resultDic[@]}
 c=$(printf "%d\n" ${a[@]} | sort -r -g )
 echo "Sorted results in descending order: " ${c[@]}
+b=$(printf "%f\n"  ${a[@]} | sort -n )
+echo "Sorted results in ascending order: " ${b[@]}
