61958cde3dd28aa09933939395a3227d49d2c60c
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-05 18:56:40 +0530

    Completed UserCase03

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
 
+#UserCase1 - That takes inputs - a, b & c
 read -p "Enter 3 number for A, B and C " a b c
 
 #UserCase02 - Compute a + b * c
 $(($a+$b*$c))
+
+#UserCase03 - Compute a * b + c
+$(($a*$b+$c))
